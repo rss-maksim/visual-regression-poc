5af112e443a56ef51e9cc8e941936c00534b772d
Maksim Kirylka <mkirylka@box.com>
2025-05-10 15:47:54 +0200

    Remove webServer in PW config

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,10 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: true,
-    timeout: 60 * 1000,
-   },
+  //  webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: true,
+  //   timeout: 60 * 1000,
+  //  },
 });
