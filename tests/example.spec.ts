2e1e0bc40672c116500da85294c525e074b588a4
Maksim Kirylka <mkirylka@box.com>
2025-05-10 13:59:06 +0200

    Set up project and PW framework

---
+++ tests/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13 Pro'],
+  locale: 'en-US',
+  geolocation: { longitude: 12.492507, latitude: 41.889938 },
+  permissions: ['geolocation'],
+})
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Virtual Regression Test POC/);
+
+  await page.screenshot({ path: 'screenshots/example.png' });
+});
+
+test('Screenshot', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+
+  page.getByLabel('Open Next.js Dev Tools').click();
+
+  await page.waitForTimeout(1000);
+
+  await page.screenshot({ path: 'screenshots/vercel.png' });
+});
